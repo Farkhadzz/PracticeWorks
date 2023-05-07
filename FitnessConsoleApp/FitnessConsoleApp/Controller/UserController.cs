e89d70193814c5496fbf1f805d05e271e62b8cea
Farkhadzz <zulfugarovfarhad6@gmail.com>
2023-05-07 20:36:04 +0400

    FitnessApp V1

---
+++ FitnessConsoleApp/FitnessConsoleApp/Controller/UserController.cs
@@ -0,0 +1,31 @@
+﻿using FitnessConsoleApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FitnessConsoleApp.Controller
+{
+    public class UserController
+    {
+        public User User { get; }
+        public UserController(string userName, string genderName, DateTime birthDay, double weight, double height)
+        {
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDay, weight, height);
+        }
+        public void Serialize()
+        {
+            var fs = new FileStream("Users.json", FileMode.OpenOrCreate);
+            JsonSerializer.Serialize(fs, User);
+        }
+        public void DeSerialize()
+        {
+            var fs = new FileStream("Users.json", FileMode.Open);
+            string json = null;
+            User user = JsonSerializer.Deserialize<User>(json);
+        }
+    }
+}
---
+++ FitnessConsoleApp/FitnessConsoleApp/Model/Gender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessConsoleApp.Model
+{
+    [Serializable]
+    public class Gender
+    {
+        public string Name { get; }
+
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentException("Name Cannot Be Empty Or 'Space'", nameof(Name));
+            }
+
+            this.Name = name;
+        }
+    }
+}
