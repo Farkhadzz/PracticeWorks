e8a75303f4e48a56bfa7c979e3bd9b1f21235524
Farkhadzz <zulfugarovfarhad6@gmail.com>
2023-06-09 13:22:47 +0400

    Added New Practice Work

---
+++ Car Wiki/Car Wiki/AddWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Car_Wiki.Model.Classes;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Car_Wiki
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public CarContext carContext;
+        public AddWindow()
+        {
+            InitializeComponent();
+            carContext = new CarContext();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string model = txtModel.Text;
+            string brand = txtBrand.Text;
+            int year = int.Parse(txtYear.Text);
+            double motor = double.Parse(txtMotor.Text);
+            int hp = int.Parse(txtHp.Text);
+            string info = txtInfo.Text;
+            if (ValidateWebsiteLink(info))
+            {
+                Car car = new Car
+                {
+                    Model = model,
+                    Brand = brand,
+                    Year = year,
+                    Motor = motor,
+                    Hp = hp,
+                    Info = info
+                };
+
+                carContext.Cars.Add(car);
+                carContext.SaveChanges();
+            }
+            else MessageBox.Show("Info Only From Sites!");
+        }
+        private bool ValidateWebsiteLink(string link)
+        {
+            string myRegex = @"^(https?://)?([\w-]+\.)+[\w-]+(/[\w-./?%&=]*)?$";
+
+            Regex regex = new Regex(myRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return regex.IsMatch(link);
+        }
+    }
+}
---
+++ Car Wiki/Car Wiki/Model/Classes/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Wiki.Model.Classes
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public double Motor { get; set; }
+        public int Hp { get; set; }
+        public string Info { get; set; }
+    }
+
+    public class CarContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=CarsDB;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+}
