de9c95ce925a2a2cac0dc8c18fd05761b2388f97
Farkhadzz <zulfugarovfarhad6@gmail.com>
2023-07-26 00:47:01 +0400

    Added QR Creator BY Farkhad

---
+++ QR Creator/QR Creator/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QR_Creator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+            QRCodeData qrData = qrCodeGenerator.CreateQrCode(QRText.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new(qrData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+           QRImage.Source = BitmapToImageSource(qrCodeImage);
+        }
+
+        private ImageSource BitmapToImageSource(Bitmap bitmap)
+        {
+            using(MemoryStream memory = new())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
